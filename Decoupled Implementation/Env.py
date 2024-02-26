d034fb1aec8eb5fef8667fa0dc9fb7e7a2ba165e
XinJingHao <75819608+XinJingHao@users.noreply.github.com>
2024-02-26 12:49:48 +0800

    Add files via upload

---
+++ Decoupled Implementation/Env.py
@@ -0,0 +1,244 @@
+import torch
+import pygame
+import numpy as np
+
+class DynamicEnv():
+    def __init__(self, opt, Predict_Scale_param):
+        self.Predict_Scale_param = Predict_Scale_param  # params[54].int(): 决定预测线段的放缩程度
+        self.dvc = opt.dvc  # 运算平台
+
+        self.window_size = opt.window_size
+        self.Search_range = opt.Search_range  # search space of the Particles
+        self.Playmode = opt.Playmode
+        self.Random_Obs = opt.RO
+        if self.Random_Obs:
+            self.Generated_Obstacle_Segments = torch.load('Maps/RMO_Obstacle_Segments.pt').to(self.dvc) #(M,2,2) or (4*O,2,2)
+        else:
+            self.Generated_Obstacle_Segments = torch.load('Maps/CMO_Obstacle_Segments.pt').to(self.dvc) #(M,2,2) or (4*O,2,2)
+        self.O = self.Generated_Obstacle_Segments.shape[0] // 4  # 障碍物数量
+        # 根据window_size对障碍物位置进行移动。注意：1) Grouped_GOS与Generated_Obstacle_Segments是联动的 2) 366是生成Maps/*.pt文件时的window_size
+        Grouped_GOS = self.Generated_Obstacle_Segments.reshape(self.O,4,2,2)
+        Grouped_GOS += (Grouped_GOS[:,0,0,:]*(opt.window_size/366-1)).unsqueeze(1).unsqueeze(1)
+
+        self.NP = int(opt.D / 2)  # 每个粒子所代表的路径的端点数量
+        self.static_obs = 2
+        self.dynamic_obs = self.O - self.static_obs
+        self.Obs_X_limit = torch.tensor([0.13,0.82], device=self.dvc)*self.window_size # [47,300] for 366
+        self.Obs_Y_limit = torch.tensor(self.Search_range, device=self.dvc)
+        self.Obstacle_V = opt.Obstacle_V # 每个障碍物x,y方向的最大速度
+        self.Target_V = opt.Target_V # 目标点的移动速度
+        self.Start_V = opt.Start_V # 被控对象的移动速度
+
+        self._Render_Init(renderPdct=opt.KP, FPS=opt.FPS)  # 渲染初始化
+
+
+    def _uniform_random(self, low, high, shape):
+        '''Generate uniformly random number in [low, high) in 'shape' on self.dvc'''
+        return (high - low)*torch.rand(shape, device=self.dvc) + low
+
+    def _get_envInfo(self):
+        '''抽取路径规划所需要的环境信息'''
+        return dict(start_point = (self.x_start, self.y_start),
+                    target_point = (self.x_target, self.y_target),
+                    d2target = self.d2target,
+                    Obs_Segments = self.Obs_Segments, # 障碍物bounding box线段，(4*O,2,2)
+                    Flat_pdct_segments = self.Flat_pdct_segments, # 障碍物运动信息线段， (dynamic_obs*4,2,2)
+                    Arrive = (self.d2target < 20), # 是否到达终点
+                    Collide = self.collide) # 是否发生碰撞
+
+    def _Render_Init(self, renderPdct, FPS):
+        pygame.init()
+        pygame.display.init()
+        self.window = pygame.display.set_mode((self.window_size, self.window_size))
+        self.clock = pygame.time.Clock()
+        self.canvas = pygame.Surface((self.window_size, self.window_size))
+
+        self.map_pyg = pygame.Surface((self.window_size, self.window_size)) # 障碍物图层
+        self.trajectory_pyg = pygame.Surface((self.window_size, self.window_size)) # 轨迹图层
+
+        # Prediction Related:
+        self.renderPdct = renderPdct # True: 画预测线, False: 画障碍物速度方向
+
+        self.FPS = FPS
+
+    def Map_Init(self):
+        self.x_start, self.y_start = 0.05*self.window_size, 0.05*self.window_size # 起点坐标, (18,18) for 366
+        self.x_target, self.y_target = 0.90*self.window_size, 0.90*self.window_size # 终点坐标, (330,330) for 366
+        if self.Playmode:
+            self.y_start = np.random.randint(0.05*self.window_size,0.90*self.window_size) # 随机起点, (18,330) for 366
+            self.y_target = np.random.randint(0.05*self.window_size, 0.90*self.window_size) # 随机终点, (18,330) for 366
+        self.d2target = ((self.x_start-self.x_target)**2 + (self.y_start-self.y_target)**2)**0.5 # distance from start to target
+
+        # 障碍物坐标：
+        self.Obs_Segments = self.Generated_Obstacle_Segments.clone()
+        self.Grouped_Obs_Segments = self.Obs_Segments.reshape(self.O,4,2,2) #注意Grouped_Obs_Segments 和 Obs_Segments 是联动的
+        # (O,4,2,2): O个障碍物，4条边，2个端点，2维坐标
+
+        # 障碍物速度:
+        self.Obs_V = self._uniform_random(1, self.Obstacle_V, (self.O, 1, 1, 2))  # 每个障碍物的x方向速度和y方向速度
+        self.Obs_V[-self.static_obs:]*=0 # 最后static_obs个障碍物不动
+        self.Obs_V[0:int(self.dynamic_obs/2)] *= -1  # 前半障碍物速度反向
+
+        # 障碍物预测线:
+        pdct_Grouped_Obs_endpoints = self.Grouped_Obs_Segments + self.Predict_Scale_param * self.Obs_V
+        self.Grouped_pdct_segments = torch.stack((self.Grouped_Obs_Segments[0:self.dynamic_obs, :, 0, :],
+                                                  pdct_Grouped_Obs_endpoints[0:self.dynamic_obs, :, 0, :]),
+                                                 dim=2)  # (dynamic_obs,4,2,2)
+        self.Flat_pdct_segments = self.Grouped_pdct_segments.reshape(self.dynamic_obs * 4, 2, 2) # 用于计算OkayPlan计算交叉点个数
+
+        # Render相关:
+        # 生成障碍物速度箭头
+        self.Grouped_Obs_center = self.Grouped_Obs_Segments.mean(axis=-3)[:, 0, :] # (O,2)
+        self.Normed_Obs_V = (self.Obs_V/((self.Obs_V**2).sum(dim=-1).pow(0.5).unsqueeze(dim=-1)+1e-8)).squeeze() # (O,2)
+        self.Grouped_Obs_Vend = self.Grouped_Obs_center + 20*self.Normed_Obs_V
+        # 初始化轨迹图层
+        self.trajectory_pyg.fill((255,255,255))
+        self.Previous_Grouped_Obs_center = None
+        self.Previous_start_point = None
+        self.Previous_target_point = None
+
+        self.collide = False
+
+        return self._get_envInfo()
+
+
+    def Update(self, path, act=None):
+        self.path = path
+
+        ''' 更新起点、终点、障碍物, 每次得到规划路径后执行一次
+            act=None时，自动更新终点; act为0~4时，根据键盘输入更新终点'''
+
+        # 起点运动:
+        if self.d2target<40: x_wp, y_wp = self.x_target, self.y_target
+        else: x_wp, y_wp = self.path[1].item(), self.path[self.NP + 1].item()
+        V_vector = torch.tensor([x_wp, y_wp]) - torch.tensor([self.x_start, self.y_start])
+        V_vector = V_vector / torch.sqrt(V_vector.pow(2).sum()+1e-6) # normalization
+        self.x_start += V_vector[0].item() * self.Start_V
+        self.y_start += V_vector[1].item() * self.Start_V
+        self.d2target = ((self.x_start-self.x_target)**2 + (self.y_start-self.y_target)**2)**0.5 # distance from start to target
+
+        # 终点运动:
+        if act is not None: # 根据键盘控制移动目标点
+            if act == 0: pass
+            elif act == 1: self.x_target -= self.Target_V
+            elif act == 2: self.y_target -= self.Target_V
+            elif act == 3: self.x_target += self.Target_V
+            elif act == 4: self.y_target += self.Target_V
+            self.x_target = np.clip(self.x_target, 0.88*self.window_size, self.window_size) # 注意0.88需要大于self.Obs_X_limit[1]的系数(0.82)
+            self.y_target = np.clip(self.y_target, self.Search_range[0],self.Search_range[1])
+        else: # 沿着y轴自动移动目标点
+            self.y_target += self.Target_V
+            if self.y_target > self.Search_range[1]:
+                self.y_target = self.Search_range[1]
+                self.Target_V *= -1
+            if self.y_target < self.Search_range[0]:
+                self.y_target = self.Search_range[0]
+                self.Target_V *= -1
+
+        # 障碍物运动
+        self.Grouped_Obs_Segments += self.Obs_V
+        Flag_Vx = ((self.Grouped_Obs_Segments[:, :, :, 0] < self.Obs_X_limit[0]) |
+                   (self.Grouped_Obs_Segments[:, :, :, 0] > self.Obs_X_limit[1])).any(dim=-1).any(dim=-1)
+        self.Obs_V[Flag_Vx, :, :, 0] *= -1
+        Flag_Vy = ((self.Grouped_Obs_Segments[:, :, :, 1] < self.Obs_Y_limit[0]) |
+                   (self.Grouped_Obs_Segments[:, :, :, 1] > self.Obs_Y_limit[1])).any(dim=-1).any(dim=-1)
+        self.Obs_V[Flag_Vy, :, :, 1] *= -1
+
+        # 随机障碍物
+        if self.Random_Obs:
+            self.Obs_V += self._uniform_random(-1, 1, (self.O, 1, 1, 2))  # 每个障碍物的x方向速度和y方向速度
+            self.Obs_V[-self.static_obs:]*=0 # 最后static_obs个障碍物不动
+            self.Obs_V.clip_(-self.Obstacle_V, self.Obstacle_V) # 限速
+
+        # 生成障碍物运动预测线
+        pdct_Grouped_Obs_endpoints = self.Grouped_Obs_Segments + self.Predict_Scale_param * self.Obs_V  # 预测位置
+        # 将当前位置和预测位置连线:
+        self.Grouped_pdct_segments = torch.stack((self.Grouped_Obs_Segments[0:self.dynamic_obs, :, 0, :],
+                                                  pdct_Grouped_Obs_endpoints[0:self.dynamic_obs, :, 0, :]),
+                                                 dim=2)  # (dynamic_obs,4,2,2)
+        self.Flat_pdct_segments = self.Grouped_pdct_segments.reshape(self.dynamic_obs * 4, 2, 2)  # 用于计算OkayPlan计算交叉点个数
+
+
+        # 生成障碍物速度箭头, 渲染
+        self.Grouped_Obs_center = self.Grouped_Obs_Segments.mean(axis=-3)[:, 0, :] # (O,2)
+        self.Normed_Obs_V = (self.Obs_V/((self.Obs_V**2).sum(dim=-1).pow(0.5).unsqueeze(dim=-1)+1e-8)).squeeze() # (O,2)
+        self.Grouped_Obs_Vend = self.Grouped_Obs_center + 20*self.Normed_Obs_V
+        self._render_frame()
+
+        return self._get_envInfo()
+
+    def _render_frame(self):
+        '''渲染+判断start_point是否与障碍物发生碰撞'''
+        Grouped_Obs_Segments = self.Grouped_Obs_Segments.cpu().int().numpy()
+        Grouped_pdct_segments = self.Grouped_pdct_segments.int().cpu().numpy()
+        Grouped_Obs_center = self.Grouped_Obs_center.int().cpu().numpy()
+        Grouped_Obs_Vend = self.Grouped_Obs_Vend.int().cpu().numpy()
+
+        # 绘制轨迹图层：
+        if self.Previous_Grouped_Obs_center is not None:
+            for _ in range(self.dynamic_obs):
+                pygame.draw.line(self.trajectory_pyg,(200, 200, 200),self.Previous_Grouped_Obs_center[_], Grouped_Obs_center[_],width=5) # 障碍物轨迹
+            pygame.draw.line(self.trajectory_pyg,(255, 174, 185),self.Previous_start_point, (self.x_start, self.y_start),width=5) # 起点轨迹
+            pygame.draw.line(self.trajectory_pyg,(202, 255, 112),self.Previous_target_point, (self.x_target, self.y_target),width=5) # 终点轨迹
+
+        self.Previous_Grouped_Obs_center = Grouped_Obs_center.copy()
+        self.Previous_start_point = (self.x_start, self.y_start)
+        self.Previous_target_point = (self.x_target, self.y_target)
+
+        # 绘制障碍物图层：
+        self.map_pyg.blit(self.trajectory_pyg, self.trajectory_pyg.get_rect())
+        for _ in reversed(range(self.O)):  # reversed: 先画静态障碍物，使其处于底层
+            # 画障碍物
+            obs_color = (25, 25, 0) if _ < self.dynamic_obs else (225, 100, 0)
+            pygame.draw.polygon(self.map_pyg, obs_color, Grouped_Obs_Segments[_, :, 0, :])
+
+        '''查看start_point是否与障碍物碰撞（在障碍物内）'''
+        oc_gd_map = (pygame.surfarray.array3d(self.map_pyg)[:, :, 2]) # Blue通道在障碍物处像素值为0
+        x, y = int(self.x_start), int(self.y_start)
+        if (oc_gd_map[x-1:x+2, y-1:y+2]==0).all(): self.collide = True # 判断点进行膨胀再判断比障碍物腐蚀再判断快
+        else: self.collide = False
+
+        for _ in range(self.O):
+            # 画预测线
+            if _ < self.dynamic_obs:
+                if self.renderPdct:
+                    for i in range(4):
+                        pygame.draw.line(self.map_pyg,(255, 0, 255),Grouped_pdct_segments[_,i,0],Grouped_pdct_segments[_,i,1],width=2)
+                else:
+                    start, end = pygame.Vector2(Grouped_Obs_center[_,0], Grouped_Obs_center[_,1]), pygame.Vector2(Grouped_Obs_Vend[_,0], Grouped_Obs_Vend[_,1])
+                    direction = end - start
+                    arrow_points = [end, end - 12 * direction.normalize().rotate(30),end - 12 * direction.normalize().rotate(-30)]
+                    pygame.draw.line(self.map_pyg, (200, 200, 200), start, end, 3)
+                    pygame.draw.polygon(self.map_pyg, (200, 200, 200), arrow_points)
+        self.canvas.blit(self.map_pyg, self.map_pyg.get_rect())
+
+        # 画路径
+        for p in range(self.NP - 1):
+            pygame.draw.line(
+                self.canvas,
+                (0, 0, 255),
+                (self.path[p].item(), self.path[p + self.NP].item()),
+                (self.path[p + 1].item(), self.path[p + self.NP + 1].item()),
+                width=4)
+
+
+        # 画起点、终点
+        pygame.draw.circle(self.canvas, (255, 0, 0), (self.x_start, self.y_start), 5)  # 起点
+        pygame.draw.circle(self.canvas, (0, 255, 0), (self.x_target, self.y_target), 5)  # 终点
+
+        # # Lead point
+        # x_wp, y_wp = self.Kinmtc[3, 0, 0, 1].item(), self.Kinmtc[3, 0, 0, self.NP + 1].item()
+        # pygame.draw.circle(self.canvas, (0, 255, 255), (x_wp, y_wp), 3)  # 终点
+
+        self.window.blit(self.canvas, self.map_pyg.get_rect())
+        pygame.event.pump()
+        pygame.display.update()
+        self.clock.tick(self.FPS)
+
+
+def str2bool(v):
+    '''Fix the bool BUG for argparse: transfer string to bool'''
+    if isinstance(v, bool): return v
+    if v.lower() in ('yes', 'True','true','TRUE', 't', 'y', '1', 'T'): return True
+    elif v.lower() in ('no', 'False','false','FALSE', 'f', 'n', '0', 'F'): return False
+    else: print('Wrong Input Type!')
---
+++ Decoupled Implementation/Planner.py
@@ -0,0 +1,239 @@
+from collections import deque
+import numpy as np
+import torch
+
+class OkayPlan():
+    '''
+    OkayPlan: A real-time path planner for dynamic environment
+    Author: Jinghao Xin
+
+    Paper Web: https://arxiv.org/abs/2401.05019
+    Code Web: https://github.com/XinJingHao/OkayPlan
+
+    Cite this algorithm:
+    @misc{OkayPlan,
+      title={OkayPlan: Obstacle Kinematics Augmented Dynamic Real-time Path Planning via Particle Swarm Optimization},
+      author={Jinghao Xin and Jinwoo Kim and Shengjia Chu and Ning Li},
+      year={2024},
+      eprint={2401.05019},
+      archivePrefix={arXiv},
+      primaryClass={cs.RO}}
+
+    Only for non-commercial purposes
+    All rights reserved
+    '''
+    def __init__(self, opt, params):
+        self.dvc = opt.dvc  # 运算平台
+
+        '''Hyperparameter Initialization'''
+        self.params = params # 该组参数的Group Number=8
+        self.G = 8 # Group数量应该严格等于生成params时的种群数量
+        # Inertia Initialization for 8 Groups:
+        self.w_init = self.params[0:self.G].unsqueeze(-1).unsqueeze(-1)
+        self.w_end = (self.params[0:self.G] * self.params[self.G:(2 * self.G)]).unsqueeze(-1).unsqueeze(-1)
+        self.Max_iterations = opt.Max_iterations  # 每帧的最大迭代次数
+        self.w_delta = (self.w_init - self.w_end) / self.Max_iterations  # (G,1,1)
+        # Velocity Initialization for 8 Groups:
+        self.v_limit_ratio = self.params[(2 * self.G):(3 * self.G)].unsqueeze(-1).unsqueeze(-1)  # (G,1,1)
+        self.v_init_ratio = 0.7 * self.v_limit_ratio  # (G,1,1)
+        # H Matrix, (4,G=8,1,1):
+        self.Hyper = torch.ones((4, self.G), device=self.dvc)
+        self.Hyper[1] = self.params[(3 * self.G):(4 * self.G)]
+        self.Hyper[2] = self.params[(4 * self.G):(5 * self.G)]
+        self.Hyper[3] = self.params[(5 * self.G):(6 * self.G)]
+        self.Hyper.unsqueeze_(-1).unsqueeze_(-1)
+
+        '''Particle Related'''
+        self.N, self.D = opt.N, opt.D # number of Groups, particles in goups, and particle dimension
+        self.arange_idx = torch.arange(self.G, device=self.dvc)  # 索引常量
+        self.Search_range = opt.Search_range  # search space of the Particles
+        self.Random = torch.zeros((4,self.G,self.N, 1), device=self.dvc)
+        self.Kinmtc = torch.zeros((4, self.G, self.N, self.D), device=self.dvc)  # [V,Pbest,Gbest,Tbest]
+        self.Locate = torch.zeros((4, self.G, self.N, self.D), device=self.dvc) # [0,X,X,X]
+
+        '''Path Related'''
+        self.NP = int(self.D/2) # 每个粒子所代表的路径的端点数量
+        self.S = self.NP-1 # 每个粒子所代表的路径的线段数量
+        self.P = self.G*self.N*self.S # 所有粒子所代表的路径的线段总数量
+        self.rd_area = 0.5 * (self.Search_range[1] - self.Search_range[0]) / (self.S)  # SEPSO中先验知识初始化粒子时的随机范围
+        self.Start_V = opt.Start_V
+
+        '''Auto Truncation'''
+        # For info, check https://arxiv.org/abs/2308.10169
+        self.AT = True
+        self.TrucWindow = 20
+
+        '''Dynamic Prioritized Initialization'''
+        self.DPI = opt.DPI
+
+    def _uniform_random(self, low, high, shape):
+        '''Generate uniformly random number in [low, high) in 'shape' on self.dvc'''
+        return (high - low)*torch.rand(shape, device=self.dvc) + low
+
+    def Priori_Path_Init(self, start, target):
+        '''在起点和终点之间均匀插值生成初始先验路径，每次导航前需要执行一次，导航中无需执行'''
+        Path_Xs = (start[0] + (target[0] - start[0]) * torch.arange(self.NP, device=self.dvc) / (self.NP - 1))
+        Path_Ys = (start[1] + (target[1] - start[1]) * torch.arange(self.NP, device=self.dvc) / (self.NP - 1))
+        self.Priori_Path = torch.cat((Path_Xs, Path_Ys))
+
+    def _fix_apex(self):
+        '''固定路径的首末端点, 注意x_start,y_start,x_target,y_target都是标量'''
+        self.Locate[1:4, :, :, 0] = self.x_start
+        self.Locate[1:4, :, :, self.NP] = self.y_start
+        self.Locate[1:4, :, :, self.NP - 1] = self.x_target
+        self.Locate[1:4, :, :, 2 * self.NP - 1] = self.y_target
+
+    def _ReLocate(self):
+        '''初始化粒子群动力学特征，每次iterate前要执行一次'''
+        # Dynamic Prioritized Initialization // Prioritized Initialization
+        if self.DPI:
+            # OkayPlan Position Init: 根据先验知识Priori_X(上次迭代的Tbest)来初始化粒子群
+            Mid_points = torch.tensor([[(self.x_start+self.x_target)/2],[(self.y_start+self.y_target)/2]],device=self.dvc).repeat((1,self.NP)).reshape(self.D) #(D,)
+            self.Locate[1:4] = Mid_points + self._uniform_random(low=-self.d2target/2, high=self.d2target/2, shape=(self.G,self.N,self.D))
+            # self.Locate[1:4, :, 0:self.params[53].int()] = Priori_X  # (3,G,RN,D), old version, not necessary
+            self.Locate[1:4, :, 0] = self.Priori_Path  # (3,G,0,D), new version, replace params[53] for 1
+        else:
+            # SEPSO Position Init: From https://github.com/XinJingHao/Real-time-Path-planning-with-SEPSO
+            RN = int(0.25*self.N)
+            self.Locate[1:4] = self._uniform_random(low=self.Search_range[0], high=self.Search_range[1], shape=(self.G,self.N,self.D)) #[0,X,X,X]
+            self.Locate[1:4, :, 0:RN] = self.Priori_Path + self._uniform_random(-self.rd_area, self.rd_area, (RN,self.D)) # (3,G,RN,D)
+
+        # 限制粒子位置至合法范围
+        self.Locate[1:4].clip_(self.Search_range[0], self.Search_range[1])
+        self._fix_apex()
+
+        # Velocity Init: (+20防止后期d2target太小时，速度太小)
+        self.Kinmtc[0] = self._uniform_random(low=-(self.d2target+20), high=(self.d2target+20), shape=(self.G,self.N,self.D)) * self.v_init_ratio
+
+        # Best Value Init:
+        self.Pbest_value = torch.ones((self.G,self.N), device=self.dvc) * torch.inf
+        self.Gbest_value = torch.ones(self.G, device=self.dvc) * torch.inf
+        self.Tbest_value = torch.inf
+
+        # Auto-Truncation Mechanism
+        self.Tbest_Collision_Free = False
+        self.Tbest_values_deque = deque(maxlen=self.TrucWindow)
+
+        # Adaptive Velocity range
+        self.v_min = -(self.v_limit_ratio * (self.d2target+20)) # (G,1,1), (+20防止后期d2target太小时，速度太小)
+        self.v_max = (self.v_limit_ratio * (self.d2target+20)) # (G,1,1), (+20防止后期d2target太小时，速度太小)
+
+    def _Cross_product_for_VectorSet(self, V_PM, V_PP):
+        '''计算 向量集V_PM 和  向量集V_PP 的交叉积 (x0*y1-x1*y0)
+            V_PM = torch.tensor((p, m, 2, 2))
+            V_PP = torch.tensor((p,2))
+            Output = torch.tensor((p, m, 2))'''
+        return V_PM[:, :, :, 0] * V_PP[:, 1, None, None] - V_PM[:, :, :, 1] * V_PP[:, 0, None, None]
+
+    def _Is_Seg_Ingersection_PtoM(self, P, M):
+        '''利用[交叉积-跨立实验]判断 线段集P 与 线段集M 的相交情况
+            P = torch.tensor((p,2,2))
+            M = torch.tensor((m,2,2))
+            Output = torch.tensor((p,m)), dtype=torch.bool'''
+
+        V_PP = P[:, 1] - P[:, 0]  # (p, 2),自身向量
+        V_PM = M - P[:, None, None, 0]  # (p, m, 2, 2), 自身起点与其他线段端点构成的向量
+        Flag1 = self._Cross_product_for_VectorSet(V_PM, V_PP).prod(dim=-1) < 0  # (p, m)
+
+        V_MM = M[:, 1] - M[:, 0]  # (m, 2)
+        V_MP = P - M[:, None, None, 0]  # (m, p, 2, 2)
+        Flag2 = self._Cross_product_for_VectorSet(V_MP, V_MM).prod(dim=-1) < 0  # (m, p)
+        return Flag1 * Flag2.T
+
+    def _get_Obs_Penalty(self):
+        # 将粒子群转化为线段，并展平为(G*N*S,2,2)
+        particles = self.Locate[1].clone()  # (G,N,D)
+        start_points = torch.stack((particles[:,:,0:(self.NP-1)], particles[:,:,self.NP:(2*self.NP-1)]), dim=-1) #(G,N,S,2), S条线段的起点坐标
+        end_points = torch.stack((particles[:,:,1:self.NP], particles[:,:,(self.NP+1):2*self.NP]), dim=-1) #(G,N,S,2), S条线段的终点坐标
+        Segments = torch.stack((start_points, end_points),dim=-2) #(G,N,S,2,2), (G,N,S)条线段的端点坐标
+        flatted_Segments = Segments.reshape((self.P,2,2)) # (G*N*S,2,2), 将所有粒子展平为G*N*S条线段
+
+        # 将展平后的粒子群线段 与 地图中的障碍物边界线段 进行跨立实验，得到交叉矩阵
+        Intersect_Matrix = self._Is_Seg_Ingersection_PtoM(flatted_Segments, self.Obs_Segments) # (P,M)
+        Current_Obs_penalty = Intersect_Matrix.sum(dim=-1).reshape((self.G,self.N,self.S)).sum(dim=-1) #(G,N)
+
+        # 将展平后的粒子群线段 与 障碍物运动轨迹线段 进行跨立实验，得到交叉矩阵
+        Pdct_Intersect_Matrix = self._Is_Seg_Ingersection_PtoM(flatted_Segments, self.Flat_pdct_segments) # (P,M)
+        Pdct_Obs_penalty = Pdct_Intersect_Matrix.sum(dim=-1).reshape((self.G, self.N, self.S)).sum(dim=-1)  # (G,N)
+        return Current_Obs_penalty, Pdct_Obs_penalty
+
+    def _get_fitness(self):
+        Segments_lenth = torch.sqrt((self.Locate[1,:,:,0:(self.NP-1)] - self.Locate[1,:,:,1:self.NP]).pow(2) +
+                             (self.Locate[1,:,:,self.NP:(2*self.NP-1)] - self.Locate[1,:,:,(self.NP+1):(2*self.NP)]).pow(2)) # (G,N,S)
+        Path_lenth = Segments_lenth.sum(dim=-1) # (G,N) # 总路径长度
+        LeadSeg_lenth = Segments_lenth[:,:,0]  # (G,N) # Start point 和 Lead point 之间的长度
+        self.Obs_penalty, Pdct_Obs_penalty = self._get_Obs_Penalty() # both in (G,N)
+
+        # Fitness = Length_Term + Collision_Penalty + Kinematics_Penalty + Lead_Point_Penalty
+        # Note that Lead_Point_Penalty is not necessary when there is a Local Planner
+        return Path_lenth +\
+               self.params[48] * self.d2target * (self.Obs_penalty) ** self.params[49] + \
+               self.params[50] * self.d2target * (Pdct_Obs_penalty) ** self.params[51] + \
+               self.params[52] * self.d2target * (LeadSeg_lenth < 1.5*self.Start_V)
+
+    def _iterate(self):
+        ''' 粒子迭代, 规划路径( 规划结果=self.Kinmtc[3,0,0] ) '''
+
+        ''' Step 0: 动态先验初始化'''
+        self._ReLocate()
+
+        for i in range(self.Max_iterations):
+            if self.AT and (i>0.2*self.TrucWindow) and self.Tbest_Collision_Free and (np.std(self.Tbest_values_deque)<10):
+                break
+
+            ''' Step 1: 计算Fitness (G,N)'''
+            fitness = self._get_fitness()
+
+            ''' Step 2: 更新Pbest, Gbest, Tbest 的 value 和 particle '''
+            # Pbest
+            P_replace = (fitness < self.Pbest_value) # (G,N)
+            self.Pbest_value[P_replace] = fitness[P_replace] # 更新Pbest_value
+            self.Kinmtc[1, P_replace] = self.Locate[1, P_replace] # 更新Pbest_particles
+
+            # Gbest
+            values, indices = fitness.min(dim=-1)
+            G_replace = (values < self.Gbest_value) # (G,)
+            self.Gbest_value[G_replace] = values[G_replace] # 更新Gbest_value
+            self.Kinmtc[2, G_replace] = (self.Locate[2, self.arange_idx, indices][G_replace].unsqueeze(1))
+
+            # Tbest
+            flat_idx = fitness.argmin()
+            idx_g, idx_n = flat_idx//self.N, flat_idx%self.N
+            min_fitness = fitness[idx_g, idx_n]
+            if min_fitness < self.Tbest_value:
+                self.Tbest_value = min_fitness # 更新Tbest_value
+                self.Kinmtc[3] = (self.Locate[3, idx_g, idx_n]).clone() #这里必须clone, 否则数据联动
+                # 查看Tbest所代表的路径是否collision free:
+                if self.Obs_penalty[idx_g, idx_n] == 0: self.Tbest_Collision_Free = True
+                else: self.Tbest_Collision_Free = False
+            self.Tbest_values_deque.append(self.Tbest_value.item())
+
+
+            ''' Step 3: 更新粒子速度 '''
+            self.Hyper[0] = self.w_init - self.w_delta*i  # 惯性因子衰减
+            self.Random[1:4] = torch.rand((3,self.G,self.N,1), device=self.dvc) #装载随机数
+            self.Kinmtc[0] = (self.Hyper * self.Random * (self.Kinmtc - self.Locate)).sum(dim=0) #(G,N,D)
+            self.Kinmtc[0].clip_(self.v_min, self.v_max) # 限制速度范围
+
+            ''' Step 4: 更新粒子位置 '''
+            self.Locate[1:4] += self.Kinmtc[0] # (3,G,N,D) + (G,N,D) = (3,G,N,D)
+            self.Locate[1:4].clip_(self.Search_range[0], self.Search_range[1]) # 限制位置范围
+            self._fix_apex()
+
+
+    def plan(self, env_info):
+        ''' 获取环境信息 -> 迭代求解路径 -> 更新下一时刻的先验路径  -> 返回当前时刻路径'''
+
+        self.x_start, self.y_start = env_info['start_point']
+        self.x_target, self.y_target = env_info['target_point']
+        self.d2target = env_info['d2target']
+        self.Obs_Segments = env_info['Obs_Segments'] # (4*O,2,2)
+        self.Flat_pdct_segments = env_info['Flat_pdct_segments'] # (dynamic_obs*4,2,2)
+
+        self._iterate()
+
+        self.Priori_Path = self.Kinmtc[3,0,0].clone()
+
+        return self.Kinmtc[3,0,0].clone(), self.Tbest_Collision_Free
+
+
